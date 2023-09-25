ce0388d6b2d642380666f7c98b5b1b0337a6dfc3
giffel <giffel123@gmail.com>
2023-09-25 14:14:58 +0200

    update #1

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Home } from'./pages/Home';
+import { Routes, Route } from 'react-router-dom';
+import './assets/styles/App.css';
+
+function App() {
+  return (
+    <>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/utils/types.tsx
@@ -0,0 +1,9 @@
+export type Story = {
+    id: number
+    title: string
+    url: string
+    time: number
+    score: number
+    by: string
+    karma: number
+}
